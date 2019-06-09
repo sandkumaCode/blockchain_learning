5d62493bfd53024c10f8d9e4bfa33dd944d220f5
Sandeep Kumar <sandkuma@ciena.com>
2019-06-09 10:43:26 +0530

    debugging for composer only

--- composer_area/run_image.sh
+++ composer_area/run_image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # replace with hostip of the interface that is public as of now bound to l0
-#docker run -it -p 127.0.0.1:8080:5000/tcp blockchainlearning/compose:1.0 /bin/sh
+docker run -it -p 127.0.0.1:8080:8000/tcp blockchainlearning/compose:1.0 /bin/sh
 # this runs the container
-docker run  -p 127.0.0.1:8080:8080/tcp -d blockchainlearning/compose:1.0
+#docker run  -p 127.0.0.1:8080:8080/tcp -d blockchainlearning/compose:1.0
