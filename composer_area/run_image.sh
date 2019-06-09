3f6c13b498652fb353b73dd52a2fb32d3993148e
Sandeep Kumar <sandkuma@ciena.com>
2019-06-09 10:32:15 +0530

    dockerfile with composer run also added expose port to run

--- composer_area/run_image.sh
+++ composer_area/run_image.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run -it blockchainlearning/compose:1.0 /bin/sh
+# replace with hostip of the interface that is public as of now bound to l0
+docker run -it -p 127.0.0.1:8080:5000/tcp blockchainlearning/compose:1.0 /bin/sh
