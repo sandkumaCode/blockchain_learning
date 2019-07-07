24a39b1705a06f790831d3a867a1c43a8cc3ce04
Sandeep Kumar <sandkuma@ciena.com>
2019-07-07 19:31:17 +0530

    Added dockerfile for composer cli + coposer rest

--- composer_area/make_image.sh
+++ composer_area/make_image.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build -t blockchainlearning/compose:1.0 .
 docker build  -t blockchainlearning/composerest:1.0 -f Dockerfile_Rest .
+docker build  -t blockchainlearning/composercli:1.0 -f Dockerfile_Cli .
--- composer_area/run_image.sh
+++ composer_area/run_image.sh
@@ -3,3 +3,4 @@
 #docker run -it -p 127.0.0.1:8080:8000/tcp blockchainlearning/compose:1.0 /bin/sh
 # this runs the container
 docker run  -p 127.0.0.1:8080:8080/tcp -d blockchainlearning/compose:1.0
+docker run  -p 127.0.0.1:3000:3000/tcp blockchainlearning/composerest:1.0
